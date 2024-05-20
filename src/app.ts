93f9e79cbdf7a1959182f821fe7dfb7ae5a4e98f
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-05-20 22:22:07 +0600

    create product done api use done

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application } from 'express';
 import cors from 'cors';
+import { ProductRouter } from './app/modules/product/product.route';
 const app: Application = express()
 
 // parsers
 app.use(express.json());
 app.use(cors());
 
+// routing 
+app.use('/api/products', ProductRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
