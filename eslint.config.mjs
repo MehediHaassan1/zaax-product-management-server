05b18289d1437f0d505d9307cd49666a02a0a97d
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-05-22 12:38:25 +0600

    simpley modified

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+
 export default [
     {
         ignores: ["**/node_modules/", ".dist/"],
         languageOptions: {
             globals: {
-                ...globals.browser,
+                ...globals.node,
                 process: "readonly",
             },
         },
--- src/app/modules/order/order.interface.ts
+++ src/app/modules/order/order.interface.ts
@@ -1,5 +1,3 @@
-import { Types } from "mongoose";
-
 export type TOrder = {
     email: string;
     productId: string;
